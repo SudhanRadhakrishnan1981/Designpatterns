702d599cef8edfdce77704560ac6aff400475908
SudhanRadhakrishnan1981 <jr.suthan@gmail.com>
2024-11-24 21:31:43 +0000

    update RabbitMQ

--- EntityFrameworkCore/EFModellayer/obj/Debug/net8.0/EFModellayer.AssemblyInfo.cs
+++ EntityFrameworkCore/EFModellayer/obj/Debug/net8.0/EFModellayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFModellayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e14cd237e9cb96223acc56978bb1739a673588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83beeb39539c41fc8a9dbbf686f9a7935aa415e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFModellayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFModellayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
